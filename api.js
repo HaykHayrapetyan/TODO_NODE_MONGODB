235c89350270663ac729bacc79ac4492965e975b
User <haykhairapetyan@gmail.com>
2017-06-21 01:09:25 +0400

    'errors'

--- api.js
+++ api.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const api = express.Router();
-const todos = [25];
+const todos = [25, 'babgn', 66];
 
 api.get('/todos', (req, res) => {
     res.json(todos);
 });
 
+api.post('/todos', (req, res) => {
+    todos.push(req.body.todo);
+    res.json(todos);
+})
+
 module.exports = api;
+
