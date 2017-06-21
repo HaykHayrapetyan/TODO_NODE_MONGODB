cfc01a9c6808bb2e57a89b0ad3584c110f9be895
Internship 5 <haykhairapetyan@gmail.com>
2017-06-21 12:44:15 +0400

    'ashxatox'

--- api.js
+++ api.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const api = express.Router();
-const todos = [25, 'babgn', 66];
+let id = 1;
+const todos = [];
+
 
 api.get('/todos', (req, res) => {
     res.json(todos);
 });
 
 api.post('/todos', (req, res) => {
-    todos.push(req.body.todo);
+    
+    todos.push({id, title: req.body.todo});
+    id++;
     res.json(todos);
 })
+
+api.delete('/todos/:id', (req, res) => {
+    for(let i = 0; i < todos.length; i++){
+        if(todos[i].id === +req.params.id){
+            todos.splice(i, 1);
+        }
+    }
+    res.json(todos);
+})
+
+api.put('/todos/:id', (req, res) => {
+    for(let i = 0; i < todos.length; i++){
+        if(todos[i].id === +req.params.id){
+            todos[i].title = req.body.title;
+        }
+    }
+    res.json(todos);
+})
+
 
 module.exports = api;
 
